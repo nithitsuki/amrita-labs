5f6521247fb0f79b3f000542e7777e03ee136b19
nithitsuki <nithilanr@gmail.com>
2025-01-28 12:37:55 +0530

    added some java files

---
+++ OOP/BMI.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.math.*;
+
+public class BMI {
+    public static void main(String[] args)
+    {
+        Scanner inpt = new Scanner(System.in);
+        System.out.print("Enter your weight (in kgs): ");
+        float weight = inpt.nextFloat();
+        System.out.println();
+        System.out.print("Enter your hreight (in cm): ");
+        float height = inpt.nextFloat();
+        System.out.println();
+        inpt.close();
+        double BMI = weight/(Math.pow((height/100),2));
+        System.out.println("YOUR BMI IS "+BMI);
+        return;
+    }
+}
---
+++ OOP/UML/LAB2_Questions.md
@@ -0,0 +1,5 @@
+# Make 2 examples for the following
++ Object   Diagram
++ Use case Diagram
++ Activity Diagram
++ Sequence Diagram
