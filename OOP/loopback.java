01c3ac075b8d144bf1710040ebb0f9cfe00e95a9
nithitsuki <nithilanr@gmail.com>
2025-01-30 22:35:21 +0530

    hf

---
+++ OOP/loopback.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class loopback {
+    public void main(String[] args) {
+        Scanner inpt = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        final String $name = inpt.nextLine();
+        System.out.print("Enter your age: ");
+        int _age = inpt.nextInt();
+        System.out.println("Welcome! "+$name+" Your "+_age+" years old");
+        inpt.close();
+        // if(P === Q)
+            // R = 1
+        // else
+            //R = 0
+        // end
+    }
+}
