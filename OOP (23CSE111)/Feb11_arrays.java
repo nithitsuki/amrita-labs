43dc624b22bf4e55aa40ddfe8804b2dcd4ed9a46
nithitsuki <nithilanr@gmail.com>
2025-02-12 12:41:43 +0530

    Added DM_LAB4, LA_LAB5 and outputs, few java files

---
+++ OOP (23CSE111)/Feb11_arrays.java
@@ -0,0 +1,19 @@
+public class Feb11_arrays{
+    public static void main(String[] args)
+    {
+        //1d arrays
+        int[]     a = new int[];          //error
+        int[]     b = new int[3];
+        //2d arrays
+        int[][]   c = new int[][];        //error
+        int[][]   d = new int[3][];
+        int[][]   e = new int[][4];       //error
+        int[][]   f = new int[3][4];
+        //3d arrays
+        int[][][] g = new int[3][4][5];
+        int[][][] h = new int[3][4][];
+        int[][][] i = new int[3][][5];    //error
+        int[][][] k = new int[][3][4];    //error
+
+    }
+}
